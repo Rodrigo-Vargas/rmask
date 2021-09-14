591a3834cad9dc4c084f3a01fcc80b401077303b
Rodrigo Vargas <rodrigovargas123@gmail.com>
2021-09-13 22:06:08 -0300

    Added missing functions

--- src/rmask.js
+++ src/rmask.js
@@ -1,5 +1,31 @@
 // =====================================================
 
+var rbrace = /^(?:\{[\w\W]*\}|\[[\w\W]*\])$/,
+	rmultiDash = /[A-Z]/g;
+
+function dataAttr( elem, key, data ) {
+	var name;
+
+	// If nothing was found internally, try to fetch any
+	// data from the HTML5 data-* attribute
+	if ( data === undefined && elem.nodeType === 1 ) {
+		name = "data-" + key.replace( rmultiDash, "-$&" ).toLowerCase();
+		data = elem.getAttribute( name );
+
+		if ( typeof data === "string" ) {
+			try {
+				data = getData( data );
+			} catch ( e ) {}
+
+			// Make sure we set the data so it isn't changed later
+			dataUser.set( elem, key, data );
+		} else {
+			data = undefined;
+		}
+	}
+	return data;
+}
+
 var _listeners = [];
 
 EventTarget.prototype.addEventListenerBase = EventTarget.prototype.addEventListener;
